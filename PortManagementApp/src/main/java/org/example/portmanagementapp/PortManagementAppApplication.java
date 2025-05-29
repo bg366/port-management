9c566246f481586ef3b8057a85512025ca41af5e
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2025-05-29 20:03:07 +0200

    feat: init project

---
+++ PortManagementApp/src/main/java/org/example/portmanagementapp/PortManagementAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.portmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortManagementAppApplication.class, args);
+    }
+
+}
